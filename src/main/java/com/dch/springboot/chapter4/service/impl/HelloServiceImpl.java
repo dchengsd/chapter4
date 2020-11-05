6f837dac108cfff9b9b63afa8a540646678f3c37
dchengsd <dchengsd@163.com>
2020-11-05 13:54:23 +0800

    test-1

--- src/main/java/com/dch/springboot/chapter4/service/impl/HelloServiceImpl.java
+++ src/main/java/com/dch/springboot/chapter4/service/impl/HelloServiceImpl.java
@@ -2,6 +2,8 @@ package com.dch.springboot.chapter4.service.impl;
 
 import com.dch.springboot.chapter4.service.HelloService;
 
+import java.sql.SQLOutput;
+
 public class HelloServiceImpl implements HelloService {
     @Override
     public void sayHello(String name) {
@@ -9,5 +11,6 @@ public class HelloServiceImpl implements HelloService {
             throw new RuntimeException("parameter is null!!");
         }
         System.out.println("hello"+name);
+        System.out.println("这是一个test!!!");
     }
 }
